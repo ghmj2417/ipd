c3418e7fc639de42672fa40d53f9bfa0e4a531fc
Moon <Josh.Moon@inin.com>
2017-05-24 10:37:50 -0400

    Move if err nil check

--- api/api.go
+++ api/api.go
@@ -69,12 +69,12 @@ func ipFromRequest(header string, r *http.Request) (net.IP, error) {
     xforwardedfor := r.Header.Get("X-Forwarded-For")
     if xforwardedfor == "" {
       host, _, err := net.SplitHostPort(r.RemoteAddr)
+      if err != nil {
+        return nil, err
+      }
     } else {
       host = xforwardedfor
     }
-		if err != nil {
-			return nil, err
-		}
 		remoteIP = host
 	}
 	ip := net.ParseIP(remoteIP)
